ec246e01dd586d6cb1bf0192fa0152d224ee022b
md adil alam <adil.alam@opalina.in>
2020-10-15 01:58:30 +0530

    add road map

---
+++ roadmap.js
@@ -0,0 +1,12 @@
+// insert value in database / api
+
+// steps
+
+// create form
+// create inputbox
+// create onchage method
+// create button
+// create onclick method
+// finally send data in api POST method
+// check response in network
+// check new updated data in ui added in list array
