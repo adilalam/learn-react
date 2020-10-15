93969f5f2372ed47b6149281c792e99710e93718
md adil alam <adil.alam@opalina.in>
2020-10-16 00:35:50 +0530

    add input box

--- src/components/Hooks.js
+++ src/components/Hooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 // class component
 // class Hooks extends Component {
@@ -11,7 +11,6 @@ import React, { useState, useEffect } from 'react'
 //         // this place for call an api for first time
 //     }
 
-
 //     // this place for render in UI (browser)
 //     render() {
 //         return (
@@ -23,26 +22,38 @@ import React, { useState, useEffect } from 'react'
 // }
 
 function Hooks() {
+  // this place for varibale initialization
+  const [username, setUsername] = useState("");
 
-    // this place for varibale initialization
-    const [data, setData] = useState(0)
+  // this place for call an api for first time
+  useEffect(() => {}, []);
 
-    // this place for call an api for first time
-    useEffect(() => { }, [])
-    
-    // use for call/invoke function
-    // ()
+  // use for call/invoke function
+  // ()
 
-    // use for doing some task inside that function, after invoking/calling
-    // () => {}
-    
+  // use for doing some task inside that function, after invoking/calling
+  // () => {}
 
-    // this place for render in UI (browser) this part also call jsx part, react compile it to HTML before rendering
-    return (
-        <div>
-            hi there
-        </div>
-    )
+  const inputChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!username) {
+      alert("Please provide text to add");
+    } else {
+      alert(username);
+    }
+  };
+
+  // this place for render in UI (browser) this part also call jsx part, react compile it to HTML before rendering
+  return (
+    <form onSubmit={submitHandler}>
+      Name <input type="text" value={username} onChange={inputChangeHandler} />
+      <button>Submit</button>
+    </form>
+  );
 }
 
-export default Hooks
+export default Hooks;
